110d167975a21f7befaa47e524ba55f56883ca93
Jayro Salazar <megajjks@gmail.com>
2020-07-03 13:05:17 -0500

    Added all components to we are register commitment: -CommitmentCard component was created to render the commitments -CommitmentCardList component was created to render a commitment list -NewCommitment component was created (preview commitment card and form to created a commitment)

--- src/component/pages/admin/Dasboard.js
+++ src/component/pages/admin/Dasboard.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import CommitmentCardList from '../../ui/CommitmentCardList';
+import Logo from '../../../assets/img/logcom.png'
 
+const data = [
+    {
+        id:"1",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'validando'
+    },
+    {
+        id:"2",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'proceso'  
+    },
+    {
+        id:"3",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'finalizado'
+    },
+    {
+        id:"4",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'oculto'
+    }
+]
 
 const Dasboard = () =>{ 
     return(
-        <h1>    
-            Dasboard
-        </h1>
+        <div>    
+            <h1>Dasboard</h1>
+            <CommitmentCardList commitments={data}/>
+        </div>
     )
 }
 
--- src/component/pages/agent/Commitment.js
+++ src/component/pages/agent/Commitment.js
@@ -1,8 +1,85 @@
 import React from 'react';
+import CommitmentCardList from '../../ui/CommitmentCardList';
+import {Link} from 'react-router-dom'
+import styled from 'styled-components';
+import Logo from '../../../assets/img/logcom.png'
+
+const Section = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+`;
+const Btn = styled(Link)`
+    text-decoration:none;
+    cursor: default;
+    background-color: ${({ theme: { colors } }) => colors.primary};
+    color: ${({ theme: { colors } }) => colors.white};
+    font-weight: 600;
+    padding:.8em;
+    transition: all 150ms ease-out;
+    &:hover{
+        cursor:pointer;
+        transform: translateY(-0.125rem);
+		box-shadow: 0 0.25rem 0.5rem 0.25rem rgba(0, 0, 0, 0.16);
+	}
+    &:active{
+        transform: translateY(-0.-125rem);
+		box-shadow: none;
+    }
+`;
+
+const data = [
+    {
+        id:"1",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'validando'
+    },
+    {
+        id:"2",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'proceso'  
+    },
+    {
+        id:"3",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'finalizado'
+    },
+    {
+        id:"4",
+        logo:Logo,
+        title:'La universidad Anáhuac Querétaro.', 
+        organization:'Anáhuac Queretaro', 
+        brief:'La Universidad Anáhuac Querétaro se compromete a capacitar a docentes provenientes del 40% de las carreras, abrir un grupo estudiantil que cree emprendimientos sociales...', 
+        location:'Querétaro', 
+        status:'oculto'
+    }
+]
+
 
 const Commitment = () =>{
+
     return(
-        <h1>Compromisos</h1>
+        <div>
+            <Section>
+                <h1>Mis compromisos</h1>
+                <Btn to='/new_commitment'>Crear un compromiso</Btn>
+            </Section>
+            <CommitmentCardList commitments={data}/>
+
+        </div>
     )
 }
 
