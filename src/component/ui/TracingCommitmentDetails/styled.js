c5f8bfc317245a53e564f12816e641f35990ab76
oscar <oscaroceguera@gmail.com>
2020-07-17 14:14:23 -0600

    Added z-index to delete btn

--- src/component/ui/TracingCommitmentDetails/styled.js
+++ src/component/ui/TracingCommitmentDetails/styled.js
@@ -1,46 +1,47 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div `
   display: grid;
   grid-template-columns: 70% auto;
   column-gap: 2em;
 `;
 
-export const WrapperTask = styled.div`
+export const WrapperTask = styled.div `
   place-self: start stretch;
 `;
 
-export const WrapperOpc = styled.div`
+export const WrapperOpc = styled.div `
   place-self: start stretch;
 `;
 
-export const WrapperColaborators = styled.div`
+export const WrapperColaborators = styled.div `
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
 `;
 
-export const Colaborator = styled.div`
+export const Colaborator = styled.div `
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-export const NameColaborator = styled.p`
+export const NameColaborator = styled.p `
   width: 10em;
   margin: 0;
   padding: 0 0.5em;
 `;
 
-export const BtnDeleteColaborator = styled.img`
+export const BtnDeleteColaborator = styled.img `
   width: 1em;
+  z-index: 99999;
   &:hover {
     cursor: pointer;
   }
 `;
 
-export const BtnAddColaborator = styled.div`
+export const BtnAddColaborator = styled.div `
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -52,13 +53,13 @@ export const BtnAddColaborator = styled.div`
   }
 `;
 
-export const BtnGroup = styled.div`
+export const BtnGroup = styled.div `
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-export const BtnSecundary = styled.p`
+export const BtnSecundary = styled.p `
   margin: 0;
   padding: 0.5em 0.8em;
   color: ${(props) => (props.primary ? "#FF5023" : "#707070")};
