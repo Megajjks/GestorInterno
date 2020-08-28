13b92b0e5323098a32888b3bd0f92f4839206244
ailed <ailedviv20@hotmail.com>
2020-08-28 13:47:59 -0500

    Se coloco los datos del compromiso en un nuevo estado dentro del modal de editar compromiso para mostrar los datos y modificar los datos que aparecen en los TextField y en los Selects. Por ultimo se crearon archivos en la carpeta de helpers donde se agregaron los arrays con el contenido que tienen los selects.

---
+++ src/helpers/commitmentImpact.js
@@ -0,0 +1,11 @@
+const commitmentImpact = [
+    'Vinculación con actores clave',
+    'Herramientas y metodologías para impulsar la innovación',
+    'social y la agencia de cambio',
+    'Asesorías especializadas',
+    'Fondos para escalar la iniciativa',
+    'Difusión y comunicación',
+    'Otro'
+]
+ 
+ export default commitmentImpact;
